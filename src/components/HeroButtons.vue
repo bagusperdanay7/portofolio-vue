9ad6192ec5661d03ba646fcf30983a7bbd0ba941
Bagus Perdana Yusuf <baguspyus@gmail.com>
2024-06-02 19:33:02 +0700

    refactor: refactor Hero Buttons Component

--- src/components/HeroButtons.vue
+++ src/components/HeroButtons.vue
@@ -7,7 +7,7 @@
         Hire Me
     </button>
     <a
-        href="/public/CV.pdf"
+        href="/CV.pdf"
         target="_blank"
         class="inline-block px-[65px] py-4 mt-4 lg:mt-0 lg:ml-[20px] font-bold text-primary-600 border border-primary-600 rounded-[10px] dark:text-primary-400 dark:border-primary-400 w-full lg:w-auto text-center"
         >View CV
