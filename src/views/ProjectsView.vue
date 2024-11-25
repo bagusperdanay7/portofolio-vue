8530daee9c6797505bad4dfc16f5315f90517307
Bagus Perdana Yusuf <baguspyus@gmail.com>
2024-11-25 23:02:09 +0700

    fix: fix project view handle null projects data

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="about h-screen">
-    <h1 class="text-pink-500">This is an about page</h1>
+    <h1 class="text-pink-500">Projects</h1>
+    <div v-if="projects.length < 1" class="dark:text-dark-100">
+      <i class="bx bx-loader bx-spin"></i> Loading...
+    </div>
+    <div v-else>{{ projects }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+};
+</script>
 
 <style></style>
